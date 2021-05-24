17e559599fe849defd4487ce561c9614adb65804
olivier <olivier@bodeswell.io>
2021-05-23 20:55:37 -0400

    update outputs

--- modules/security_group/default/output.tf
+++ modules/security_group/default/output.tf
@@ -1,52 +1,52 @@
 ### Egress ###
 output "sg_egress_id" {
   description = "The ID of sg_egress"
-  value       = module.sg_egress.this_security_group_id
+  value       = module.sg_egress.security_group_id
 }
 
 output "sg_egress_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.sg_egress.this_security_group_vpc_id
+  value       = module.sg_egress.security_group_vpc_id
 }
 
 output "sg_egress_group_owner_id" {
   description = "The owner ID"
-  value       = module.sg_egress.this_security_group_owner_id
+  value       = module.sg_egress.security_group_owner_id
 }
 
 output "sg_egress_group_name" {
   description = "The name of the security group"
-  value       = module.sg_egress.this_security_group_name
+  value       = module.sg_egress.security_group_name
 }
 
 output "sg_egress_group_description" {
   description = "The description of the security group"
-  value       = module.sg_egress.this_security_group_description
+  value       = module.sg_egress.security_group_description
 }
 
 
 ### SSH ###
 output "sg_ssh_22_id" {
   description = "The ID of sg_ssh_22"
-  value       = module.sg_ssh_22.this_security_group_id
+  value       = module.sg_ssh_22.security_group_id
 }
 
 output "sg_ssh_22_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.sg_ssh_22.this_security_group_vpc_id
+  value       = module.sg_ssh_22.security_group_vpc_id
 }
 
 output "sg_ssh_22_group_owner_id" {
   description = "The owner ID"
-  value       = module.sg_ssh_22.this_security_group_owner_id
+  value       = module.sg_ssh_22.security_group_owner_id
 }
 
 output "sg_ssh_22_group_name" {
   description = "The name of the security group"
-  value       = module.sg_ssh_22.this_security_group_name
+  value       = module.sg_ssh_22.security_group_name
 }
 
 output "sg_ssh_22_group_description" {
   description = "The description of the security group"
-  value       = module.sg_ssh_22.this_security_group_description
+  value       = module.sg_ssh_22.security_group_description
 }
--- modules/security_group/tf2/output.tf
+++ modules/security_group/tf2/output.tf
@@ -1,25 +1,25 @@
 ### tf2 ###
 output "sg_tf2_id" {
   description = "The ID of sg_tf2"
-  value       = module.sg_tf2.this_security_group_id
+  value       = module.sg_tf2.security_group_id
 }
 
 output "sg_tf2_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.sg_tf2.this_security_group_vpc_id
+  value       = module.sg_tf2.security_group_vpc_id
 }
 
 output "sg_tf2_group_owner_id" {
   description = "The owner ID"
-  value       = module.sg_tf2.this_security_group_owner_id
+  value       = module.sg_tf2.security_group_owner_id
 }
 
 output "sg_tf2_group_name" {
   description = "The name of the security group"
-  value       = module.sg_tf2.this_security_group_name
+  value       = module.sg_tf2.security_group_name
 }
 
 output "sg_tf2_group_description" {
   description = "The description of the security group"
-  value       = module.sg_tf2.this_security_group_description
+  value       = module.sg_tf2.security_group_description
 }
